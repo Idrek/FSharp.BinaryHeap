b9085ffa45ceb5cf7726575064f2b03a5386468e
Idrek <iddarg@gmail.com>
2020-04-12 17:42:35 +0200

    New `Core.shiftUpOrder` function

--- src/Core.fs
+++ src/Core.fs
@@ -1,6 +1,20 @@
 ﻿module BinaryHeap.Core
 
+type BinaryHeap<'a> = array<'a>
+type Order = Min | Max
+
 let private isEven (n: int) : bool = n &&& 1 = 0
+
+let private swap (x: int) (y: int) (arr: array<'a>) : array<'a> =
+    let length = Array.length arr
+    match length with
+    | 0 -> ()
+    | length when x >= length || y >= length -> ()
+    | _ ->
+        let aux : 'a = arr.[x]
+        arr.[x] <- arr.[y]
+        arr.[y] <- aux 
+    arr
 
 let findChildrenIndexes (iLast: int) (iParent: int) : (Option<int> * Option<int>) =
     let checkBoundary (iChild: int) = if iChild > iLast then None else Some iChild 
@@ -14,4 +28,26 @@ let findParentIndex (iChild: int) : Option<int> =
     | 0 -> None
     | iChild when isEven iChild -> Some <| (iChild - 1) / 2
     | iChild -> Some <| iChild / 2
+
+let empty : BinaryHeap<'a> = Array.empty
+
+let rec shiftUpOrder (order: Order) (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
+    let childParentComparison : 'a -> 'a -> bool = 
+        match order with 
+        | Min -> (<=) 
+        | Max -> (>=)
+    match iItem, heap with
+    | _, [||] -> empty
+    | iItem, heap when iItem <= 0 -> heap
+    | iItem, heap when iItem >= Array.length heap -> heap
+    | iItem, heap ->
+        let iParent : Option<int> = findParentIndex iItem
+        match iParent with
+        | None -> heap
+        | Some iParent -> 
+            if childParentComparison heap.[iParent] heap.[iItem]
+            then heap
+            else 
+                let newHeap : BinaryHeap<'a> = swap iParent iItem heap
+                shiftUpOrder order iParent newHeap
 
