64c61d8db7a6e253dce88ee1f257b07ff7e2254a
Idrek <iddarg@gmail.com>
2020-04-12 17:30:04 +0200

    Tests for `Core.findParentIndex` function

--- test/CoreTests.fs
+++ test/CoreTests.fs
@@ -11,3 +11,14 @@ let ``Test findChildrenIndexes`` () =
     Assert.Equal((Some 5, None), findChildrenIndexes 5 2)
     Assert.Equal((None, None), findChildrenIndexes 4 2)
 
+[<Fact>]
+let ``Test findParentIndex`` () =
+    Assert.Equal(None, findParentIndex 0)
+    Assert.Equal(Some 0, findParentIndex 1)
+    Assert.Equal(Some 0, findParentIndex 2)
+    Assert.Equal(Some 1, findParentIndex 3)
+    Assert.Equal(Some 1, findParentIndex 4)
+    Assert.Equal(Some 2, findParentIndex 5)
+    Assert.Equal(Some 3, findParentIndex 7)
+
+    
