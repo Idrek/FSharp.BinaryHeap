4d022de09a244789e58beb1fe7085f1b783ce866
Idrek <iddarg@gmail.com>
2020-04-12 17:44:52 +0200

    New `Min.shiftUp` function

---
+++ src/Min.fs
@@ -0,0 +1,8 @@
+module BinaryHeap.Min
+
+type BinaryHeap<'a> = Core.BinaryHeap<'a>
+type Order = Core.Order
+
+let shiftUp (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
+    Core.shiftUpOrder Order.Min iItem heap
+    
