c5dddef1c0203f8ccacbcad71b21b0e82aee6dfd
Idrek <iddarg@gmail.com>
2020-04-12 19:22:46 +0200

    New `Max.pop` function

--- src/Max.fs
+++ src/Max.fs
@@ -12,3 +12,6 @@ let push (item: 'a) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
 let shiftDown (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
     Core.shiftDownOrder Order.Max iItem heap
 
+let pop (heap: BinaryHeap<'a>) : Option<'a * BinaryHeap<'a>> =
+    Core.popOrder Order.Max heap
+
