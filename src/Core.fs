2d0809dbb14f9bf36605e94f1f84a32c01ba071d
Idrek <iddarg@gmail.com>
2020-04-12 17:22:06 +0200

    New `Core.findChildrenIndexes` function

--- src/Core.fs
+++ src/Core.fs
@@ -1,5 +1,8 @@
-﻿namespace BinaryHeap
+﻿module BinaryHeap.Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+let findChildrenIndexes (iLast: int) (iParent: int) : (Option<int> * Option<int>) =
+    let checkBoundary (iChild: int) = if iChild > iLast then None else Some iChild 
+    let double : int = iParent * 2
+    let iLeftChild : int = double + 1
+    let iRightChild : int = double + 2
+    (checkBoundary iLeftChild, checkBoundary iRightChild)
