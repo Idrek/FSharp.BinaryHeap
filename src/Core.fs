d224d07f61f9215e3f49f78e958a05c0c75d58a2
Idrek <iddarg@gmail.com>
2020-04-12 19:28:03 +0200

    New `Core.heapifyOrder` function

--- src/Core.fs
+++ src/Core.fs
@@ -99,3 +99,11 @@ let popOrder (order: Order) (heap: BinaryHeap<'a>) : Option<'a * BinaryHeap<'a>>
         let fixedHeap : BinaryHeap<'a> = shiftDown 0 heapWithoutLastItem
         Some (poppedItem, fixedHeap)
 
+let heapifyOrder (order: Order) (arr: array<'a>) : BinaryHeap<'a> =
+    let shiftUp : int -> BinaryHeap<'a> -> BinaryHeap<'a> = 
+        match order with | Min -> shiftUpOrder Min | Max -> shiftUpOrder Max
+    let length : int = Array.length arr
+    // TODO: Is necessary traverse the whole range?
+    let range : array<int> = [|1 .. length - 1|]
+    (arr, range) ||> Array.fold (fun heap iItem -> shiftUp iItem heap)
+
