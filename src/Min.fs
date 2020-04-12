c737faf3378e2bd6802addea6a2a68ff75d989ae
Idrek <iddarg@gmail.com>
2020-04-12 17:57:53 +0200

    New `Min.push` function

--- src/Min.fs
+++ src/Min.fs
@@ -5,4 +5,7 @@ type Order = Core.Order
 
 let shiftUp (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
     Core.shiftUpOrder Order.Min iItem heap
-    
+
+let push (item: 'a) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
+    Core.pushOrder Order.Min item heap
+
