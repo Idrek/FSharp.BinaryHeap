a4f8719e1411e443d1b1dc95921c021a552a4b78
Idrek <iddarg@gmail.com>
2020-04-28 21:24:18 +0200

    Refactor tests of `Min.shiftUp` function
    - Rename title to `shiftUpInPlace`.
    - Test the in-place version.

--- test/MinTests.fs
+++ test/MinTests.fs
@@ -6,35 +6,45 @@ open Xunit
 type BinaryHeap<'a> = BinaryHeap.Core.BinaryHeap<'a>
 
 [<Fact>]
-let ``Test shiftUp`` () =
+let ``Test shiftUpInPlace`` () =
     let emptyHeap : BinaryHeap<int> = Array.empty
-    Assert.Equal<BinaryHeap<int>>(emptyHeap, shiftUp 0 emptyHeap)
-    Assert.Equal<BinaryHeap<int>>(emptyHeap, shiftUp 6 emptyHeap)
+    shiftUpInPlace 0 emptyHeap
+    Assert.Empty(emptyHeap)
+    shiftUpInPlace 6 emptyHeap
+    Assert.Empty(emptyHeap)
 
     let heap : BinaryHeap<int> = [|1|]
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 0 heap)
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 1 heap)
+    shiftUpInPlace 0 heap
+    Assert.Equal<BinaryHeap<int>>([|1|], heap)
+    shiftUpInPlace 1 heap
+    Assert.Equal<BinaryHeap<int>>([|1|], heap)
 
     let heap : BinaryHeap<int> = [|1; 2|]
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 0 heap)
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 1 heap)
+    shiftUpInPlace 0 heap
+    Assert.Equal<BinaryHeap<int>>([|1; 2|], heap)
+    shiftUpInPlace 1 heap
+    Assert.Equal<BinaryHeap<int>>([|1; 2|], heap)
 
     let heap : BinaryHeap<int> = [|2; 1|]
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 0 heap)
-    Assert.Equal<BinaryHeap<int>>([|1; 2|], shiftUp 1 heap)
+    shiftUpInPlace 0 heap
+    Assert.Equal<BinaryHeap<int>>([|2; 1|], heap)
+    shiftUpInPlace 1 heap
+    Assert.Equal<BinaryHeap<int>>([|1; 2|], heap)
 
     let heap : BinaryHeap<int> = [|4; 4; 8; 9; 4; 12; 9; 11; 13; 7; 10; 5|]
     let iLast : int = Array.length heap - 1
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp -4 heap)
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 0 heap)
+    shiftUpInPlace -4 heap
+    Assert.Equal<BinaryHeap<int>>([|4; 4; 8; 9; 4; 12; 9; 11; 13; 7; 10; 5|], heap)
+    shiftUpInPlace 0 heap
+    Assert.Equal<BinaryHeap<int>>([|4; 4; 8; 9; 4; 12; 9; 11; 13; 7; 10; 5|], heap)
     // The unique element that violates heap rules is the last one, so
     // any other will return the same heap unmodified.
-    Assert.Equal<BinaryHeap<int>>(heap, shiftUp 7 heap)
+    shiftUpInPlace 7 heap
+    Assert.Equal<BinaryHeap<int>>([|4; 4; 8; 9; 4; 12; 9; 11; 13; 7; 10; 5|], heap)
     // Progress: Swap the 5 with its parent (12), and then again with 
     // its new parent (8).
-    Assert.Equal<BinaryHeap<int>>(
-        [|4; 4; 5; 9; 4; 8; 9; 11; 13; 7; 10; 12|], 
-        shiftUp iLast heap)
+    shiftUpInPlace iLast heap
+    Assert.Equal<BinaryHeap<int>>([|4; 4; 5; 9; 4; 8; 9; 11; 13; 7; 10; 12|], heap)
 
 [<Fact>]
 let ``Test push`` () =
