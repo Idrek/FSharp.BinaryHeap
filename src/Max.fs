ee72ebe5fefb70629dd015252af5d686590833c6
Idrek <iddarg@gmail.com>
2020-04-28 21:23:26 +0200

    Refactor `Max.shiftDown` function
    - Rename to `shiftDownInPlace`.
    - Adapt code to use in-place version of functions.

--- src/Max.fs
+++ src/Max.fs
@@ -9,8 +9,8 @@ let shiftUpInPlace (iItem: int) (heap: BinaryHeap<'a>) : unit =
 let push (item: 'a) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
     Core.pushOrder Order.Max item heap
 
-let shiftDown (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
-    Core.shiftDownOrder Order.Max iItem heap
+let shiftDownInPlace (iItem: int) (heap: BinaryHeap<'a>) : unit =
+    Core.shiftDownOrderInPlace Order.Max iItem heap
 
 let pop (heap: BinaryHeap<'a>) : Option<'a * BinaryHeap<'a>> =
     Core.popOrder Order.Max heap
