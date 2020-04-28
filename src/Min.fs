5e3d205f29669272c47e1c87c67526ee3ef4672f
Idrek <iddarg@gmail.com>
2020-04-28 21:20:40 +0200

    Refactor `Min.shiftUp` function
    - Rename to `shiftUpInPlace`.
    - Adapt code to use in-place functions.

--- src/Min.fs
+++ src/Min.fs
@@ -3,8 +3,8 @@ module BinaryHeap.Min
 type BinaryHeap<'a> = Core.BinaryHeap<'a>
 type Order = Core.Order
 
-let shiftUp (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
-    Core.shiftUpOrder Order.Min iItem heap
+let shiftUpInPlace (iItem: int) (heap: BinaryHeap<'a>) : unit =
+    Core.shiftUpOrderInPlace Order.Min iItem heap
 
 let push (item: 'a) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
     Core.pushOrder Order.Min item heap
