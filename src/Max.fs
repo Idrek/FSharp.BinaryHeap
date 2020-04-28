e07ea5b1d73e23dc257d515c8e8c3d668317874c
Idrek <iddarg@gmail.com>
2020-04-28 21:22:53 +0200

    Refactor `Max.shiftUp` function
    - Rename to `shiftUpInPlace`.
    - Adapt code to use in-place version of functions.

--- src/Max.fs
+++ src/Max.fs
@@ -3,8 +3,8 @@ module BinaryHeap.Max
 type BinaryHeap<'a> = Core.BinaryHeap<'a>
 type Order = Core.Order
 
-let shiftUp (iItem: int) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
-    Core.shiftUpOrder Order.Max iItem heap
+let shiftUpInPlace (iItem: int) (heap: BinaryHeap<'a>) : unit =
+    Core.shiftUpOrderInPlace Order.Max iItem heap
 
 let push (item: 'a) (heap: BinaryHeap<'a>) : BinaryHeap<'a> =
     Core.pushOrder Order.Max item heap
